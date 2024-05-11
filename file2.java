9646cc142d93e24682eee6c770158c29042e6d7f
Marri brahmaiah <karthik23012019@gmail.com>
2024-05-11 17:16:32 +0530

    my second java programe

---
+++ file2.java
@@ -0,0 +1,7 @@
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Csecond java
